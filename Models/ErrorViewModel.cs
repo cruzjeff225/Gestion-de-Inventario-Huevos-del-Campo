f583e155bcaa583c5708e15fd4837a2744da508a
cruzjeff225 <cruzjeff225@gmail.com>
2025-05-18 21:20:56 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gestión_de_Inventario_Huevos_del_Campo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
